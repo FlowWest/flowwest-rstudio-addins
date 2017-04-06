e8495612e50dd92400afc86393b601c5be22c419
ergz <e.emanuel.rodriguez@gmail.com>
2017-04-06 14:24:29 -0700

    not in working state

--- R/createTODOAddin.R
+++ R/createTODOAddin.R
@@ -1,18 +1,56 @@
-#' Create a Todo markdown based on @TODO tags in file 
-#' 
-#' Call this as an addin to create TODO list for a given script.
-#' 
+#' @import miniUI shiny magrittr
+
 #' @export
-createTODO <- function() {
+createTODOAddin <- function() {
+  ui <- miniPage(
+    gadgetTitleBar("Todo Builder"),
+    miniContentPanel(
+      
+      tableOutput(outputId = "todo_output")
+    )
+  )
   
+  server <- function(input, output, session) {
+    current_document <- rstudioapi::getActiveDocumentContext()
+    document_content <- trimws(current_document$content)
+    
+    todo_tags <- find_TODO_tags(document_content)
+    lines_with_tag <- document_content[todo_tags]
+    todo_topics <- purrr::map(todo_tags, function(x) {
+      get_topic_from_tag(x)
+    }) %>% 
+      purrr::flatten_chr()
+    
+    todo_messages <- purrr::map(todo_tags, function(x) {
+      get_message_from_tag(x)
+    }) %>% 
+      purrr::flatten_chr()
+    
+    output$todo_output <- renderTable({
+      data.frame(todo_topics=todo_topics, todo_messages=todo_messages)
+    })
+  }
+  
+  viewer <- dialogViewer("TODO Builder", width = 1000, height = 800)
+  runGadget(ui, server, viewer = viewer)
 }
 
-#@TODO:
-    #@TODO:
-find_TODO_tags <- function(){
-  current_document <- rstudioapi::getActiveDocumentContext()
-  document_content <- trimws(current_document$content)
-  
-  lines_with_tag <- which(stringr::str_detect(document_content, "^#@TODO:"))
+find_TODO_tags <- function(doc_content){
+  doc_content <- trimws(doc_content)
+  lines_with_tag <- which(stringr::str_detect(doc_content, "^#@TODO:"))
+
+  return(lines_with_tag)  
 }
+
+get_topic_from_tag <- function(todo_line) {
+  stringr::str_extract(todo_line, "TODO:[A-Za-z]+ ") %>% 
+    stringr::str_replace("TODO:", "")
+}
+
+get_message_from_tag <- function(todo_line) {
+  stringr::str_extract(todo_line, "---(.)+") %>% 
+    stringr::str_replace("---", "") %>% 
+    trimws()
+}
+
   
